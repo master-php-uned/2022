61b92f6bed31ef9152216a101881f9c4b537a14f
Jesus Martin Blanco <jesusmartinb64@gmail.com>
2022-02-10 17:43:45 +0100

    Terminamos el desarrollo de la interfaz de login y de la función Index del archivo www/Controllers/IndexController.php

--- www/Controllers/IndexController.php
+++ www/Controllers/IndexController.php
@@ -8,8 +8,29 @@ class IndexController extends Controller
 
 	// creamos un método de acción 
 	public function Index(){
-		// con el objeto que contiene la instancia de View llamamos al método Render que recibe como parámetros el nombre de la clase y el nombre de una vista, así como las variables de sesión
-		$this->view->Render($this,"index",null,null,null);
+
+		// Obtenemos información de las variables de session
+		$model1 = Session::getSession("model1");
+		$model2 = Session::getSession("model2");
+		// comprobamos que contengan su información correspondiente
+		if(null != $model1 || null != $model2){
+			// deserializamos la información que contienen
+			$array1 = unserialize($model1);
+			$array2 = unserialize($model2);
+			// comprobamos que sean arrays
+			if(is_array($array1) && is_array($array2)){
+				// Inicializamos los objetos $model1 y $model2 con la información que nos proporciona la función TUser a la que se pasa como parámetro los objetos $array1 y $array2
+				$model1 = $this->TUser($array1);
+				$model2 = $this->TUser($array2);
+				// pasamos al método Render la información para poder pasar la información a la vista de login
+				this->view->Render($this,"index",$model1,$model2,null);
+			}else{
+				$this->view->Render($this,"index",null,null,null);
+			}
+		}else{
+			// con el objeto que contiene la instancia de View llamamos al método Render que recibe como parámetros el nombre de la clase y el nombre de una vista, así como las variables de sesión
+			$this->view->Render($this,"index",null,null,null);
+		}
 	}
 }
 
--- www/Views/Index/index.php
+++ www/Views/Index/index.php
@@ -1,27 +1,27 @@
 	<div class="mx-auto w-25 border border-2 rounded-2 p-2 m-2 mb-5" id="login">
 		<h3 class="text-center mt-2">Login</h3>
-		<form method="post" action="Index/Login" name="login">
+		<form method="POST" action="Index/Login" name="login">
 			<div class="row mb-3">
 				<label for="email" class="col-sm-12 col-form-label">Email</label>
 				<div class="col-sm-12">
-					<input type="email" class="form-control" name="email" id="email" autocomplete="off" placeholder="Email" value="<?php //echo ... ?>" >
+					<input type="email" class="form-control" name="email" id="email" autocomplete="off" placeholder="Email" value="<?php echo $model1->Email ?? "" ?>" onkeypress="new User().ClearMessages(this);">
 					<span id="emails" class="text-danger">
-						<?php //echo ... ?>
+						<?php echo $model2->Email ?? "" ?>
 					</span>
 				</div>
 			</div>
 			<div class="row mb-3">
 				<label for="password" class="col-sm-12 col-form-label">Password</label>
 				<div class="col-sm-12">
-					<input type="password" class="form-control" name="password" id="password" autocomplete="off" placeholder="Password" value="<?php //echo ... ?>" >
+					<input type="password" class="form-control" name="password" id="password" autocomplete="off" placeholder="Password" value="<?php echo $model1->Password ?? "" ?>" >
 					<span id="passwords" class="text-danger">
-						<?php //echo ... ?>
+						<?php echo $model2->Password ?? "" ?>
 					</span>
 				</div>
 			</div>
 			<div class="row mb-3">
 				<span class="text-danger">
-					<?php // echo ... ?>
+					<?php echo $model2->Password ?? "" ?>
 				</span>
 			</div>
 			<input type="submit" class="btn btn-info btn-block" name="loginSubmit" value="Login">
