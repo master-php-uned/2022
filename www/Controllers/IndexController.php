443d53a9faa91adf1e0827cc6145b2ed072976c1
Jesus Martin Blanco <jesusmartinb64@gmail.com>
2022-02-11 22:52:28 +0100

    Se obtiene la información del formulario de login por el servidor a traves de la función Login en el fichero www/Controllers/IndexController.php

--- www/Controllers/IndexController.php
+++ www/Controllers/IndexController.php
@@ -23,7 +23,7 @@ class IndexController extends Controller
 				$model1 = $this->TUser($array1);
 				$model2 = $this->TUser($array2);
 				// pasamos al método Render la información para poder pasar la información a la vista de login
-				this->view->Render($this,"index",$model1,$model2,null);
+				$this->view->Render($this,"index",$model1,$model2,null);
 			}else{
 				$this->view->Render($this,"index",null,null,null);
 			}
@@ -31,6 +31,48 @@ class IndexController extends Controller
 			// con el objeto que contiene la instancia de View llamamos al método Render que recibe como parámetros el nombre de la clase y el nombre de una vista, así como las variables de sesión
 			$this->view->Render($this,"index",null,null,null);
 		}
+	}
+
+	// Obtenemos la información del login por el servidor
+	public function Login(){
+		$execute = true;
+		// verificamos el email
+		if(empty($_POST["email"])){
+			$email = "Enter your email";
+			$execute = false;
+		}else{
+			// Si el email contiene datos validamos que sea un email valido
+			if(!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)){
+				$email = "Enter a valid email";
+				$execute = false;
+			}
+		}
+		// verificamos el password
+		if(empty($_POST["password"])){
+			$password = "Enter a password";
+			$execute = false;
+		}
+		// creamos el objeto $model1 inicializado como un array
+		$model1 = array(
+			// contiene dos llaves que obtenemos por POST de nuestro formulario
+			"Email"=>$_POST["email"] ?? null,
+			"Password"=>$_POST["password"] ?? null,
+		);
+		// Serializamos la información para almacenarla en la variable de session $model1
+		Session::setSession('model1',serialize($model1));
+		// evaluamos el objeto $execute
+		if($execute){
+
+		}else{
+			// creamos una variable de session model2 y serializamos un array que contiene los mensajes de error
+			Session::setSession('model2',serialize(array(
+				"Email"=>$email ?? null,
+				"Password"=>$password ?? null,
+			)));
+			header('Location: '.URL);
+
+		}
+
 	}
 }
 
