00d3bd92aa709957a9d43ef5fecf09e2c711d3d3
Jesús Martín Blanco <jesusmartinb64@gmail.com>
2022-04-10 10:47:08 +0200

    Se modifica el archivo adminnav.blade.php para resaltar la opción activa del menú de navegación. Se crea el formulario para crear un nuevo usuariogenerandose el archivo resources/views/users/create.blade.php

--- resources/views/ui/adminnav.blade.php
+++ resources/views/ui/adminnav.blade.php
@@ -1,2 +1,3 @@
-<a href="" class="text-white text-sm uppercase font-bold p-3">Ver Usuarios</a>
-<a href="" class="text-white text-sm uppercase font-bold p-3">Nuevo Usuario</a>
+{{-- Podemos resaltar el enlace activo con Request::is indica desde donde viene el request es decir la llamada --}}
+<a href="{{ route('users.index') }}" class="text-white text-sm uppercase font-bold p-3 {{ Request::is('users') ? 'bg-teal-500' : '' }}">Ver Usuarios</a>
+<a href="{{ route('users.create') }}" class="text-white text-sm uppercase font-bold p-3 {{ Request::is('users/create') ? 'bg-teal-500' : '' }}">Nuevo Usuario</a>
