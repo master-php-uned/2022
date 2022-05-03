9db8fe9b11ac5a56987fbd8675ebf047ba20248b
Jesús Martín Blanco <jesusmartinb64@gmail.com>
2022-05-03 20:38:19 +0200

    Se crea la migración create_listas_table y se migra

---
+++ src/database/migrations/2022_05_02_195907_create_listas_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateListasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('listas', function (Blueprint $table) {
+            $table->id();
+            $table->string('listid', 36)->unique();
+            $table->string('nombre', 200);
+            $table->text('descripcion')->nullable();
+            $table->dateTime('fecha')->nullable();
+            $table->string('imagen', 150)->nullable();
+            $table->foreignId('idcanal')->constrained('canals')->onUpdate('cascade')->onDelete('cascade');
+            $table->datetime('actualizado')->default("1000-01-01 00:00:00");
+            $table->string('etagDatos', 28)->nullable();
+            $table->string('etagVideos', 28)->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('listas');
+    }
+}
